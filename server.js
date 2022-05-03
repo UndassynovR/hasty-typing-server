ac28be15a5f5fbf3f6baccb77b375b1d7b845476
Magnus Philosophus <ramilendo200320@gmail.com>
2022-05-03 20:35:31 +0200

    get test and  react routes

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const express = require('express')
 const app = express()
 app.use(express.static(path.join(__dirname, '../client/build')))
 
-app.get('/', (req, res) => {
-    res.sendFile(express.static(path.join(__dirname, '../client/build/index.html')))
+let words = [
+    {id: 1, word: 'hello'},
+    {id: 2, word: 'hello2'},
+    {id: 3, word: 'hello3'}
+]
+app.get('/api/words', (req, res) => {
+    console.log('api/words fetched');
+    res.json(words)
+})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 app.listen(config.get('port'), () => {
     console.log(`Server works on port ${config.get('port')}`)
